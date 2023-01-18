7655b3723fcb647e71dfc774f14e7ffdae2d0b97
James Stine <James Stine>
2023-01-18 07:50:08 -0600

    tweak

--- lab0/regfile.sv
+++ lab0/regfile.sv
@@ -1,10 +1,10 @@
-module regfile(input logic         clk, 
-               input logic 	   we3, 
-               input logic [4:0]   ra1, ra2, wa3, 
-               input logic [31:0]  wd3, 
-               output logic [31:0] rd1, rd2);
+module regfile (input logic         clk, 
+		input logic 	    we3, 
+		input logic [4:0]   ra1, ra2, wa3, 
+		input logic [31:0]  wd3, 
+		output logic [31:0] rd1, rd2);
    
-   logic [31:0] 		   rf[31:0];
+   logic [31:0] 		    rf[31:0];
    
    // three ported register file
    // read two ports combinationally
