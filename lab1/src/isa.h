1edd1ebdfc22612041ea056661afc0ef0ff6e42e
James Stine <James Stine>
2023-02-14 15:51:29 -0600

    Slight typo from previous version

--- lab1/src/isa.h
+++ lab1/src/isa.h
@@ -45,7 +45,7 @@ int BNE (int Rs1, int Rs2, int Imm, int Funct3) {
   int cur = 0;
   Imm = Imm << 1;
   if (CURRENT_STATE.REGS[Rs1] != CURRENT_STATE.REGS[Rs2])
-    NEXT_STATE.PC = (CURRENT_STATE.PC + 4) + (SIGNEXT(Imm,13));
+    NEXT_STATE.PC = (CURRENT_STATE.PC - 4) + (SIGNEXT(Imm,13));
   return 0;
 
 }
